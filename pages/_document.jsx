f5a0311e57b3273b068b088d3c85561439b2444e
Bruno Bulnes <bulnes.bruno@gmail.com>
2023-10-28 02:28:27 -0300

    feat: add styled components lib

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,11 +1,15 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { Footer } from "../page-components/footer/footer";
+import { Header } from "../page-components/header/header";
 
 export default function Document() {
   return (
     <Html>
       <Head />
       <body>
+        <Header />
         <Main />
+        <Footer />
         <NextScript />
       </body>
     </Html>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+`;
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+      <Title>Hello, Next.js!</Title>
+
+      <Link href="/about">About</Link>
+    </>
+  );
 }
