240ca498815a6930ecff615a1dedbe5f360d100d
Bruno Bulnes <bulnes.bruno@gmail.com>
2023-10-30 02:17:08 -0300

    chore: remove unused hook

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,12 +1,9 @@
 import { Head, Html, Main, NextScript } from "next/document";
-import { usePathname } from "next/navigation";
 import { Footer } from "../components/footer/footer";
 import { Header } from "../components/header/header";
 import { ToolBar } from "../components/toolbar/toolbar";
 
 export default function Document() {
-  const pathname = usePathname();
-
   return (
     <Html lang="pt-br">
       <Head />
