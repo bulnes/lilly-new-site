16245a39380aa16660895146426a9d8b8194b9f3
Bruno Bulnes <bulnes.bruno@gmail.com>
2023-11-03 17:32:22 -0300

    fix: max width on desktop container

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -1,3 +1,3 @@
 export function Container({ children }) {
-  return <div className="px-5 mx-auto lg:px-0 lg:max-w-8xl">{children}</div>;
+  return <div className="px-5 mx-auto lg:px-0 lg:max-w-7xl">{children}</div>;
 }
